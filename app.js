63c505ef9ee9dce87140fe2a90459577f95219f7
Engines-screaming <karl.ravago@gmail.com>
2021-09-20 19:40:51 -0700

    created js file to control application. section in page added for memes

---
+++ app.js
@@ -0,0 +1,10 @@
+// selectors
+const image = document.querySelector("imgLink");
+const topText = document.querySelector("topText");
+const botText = document.querySelector("botText");
+const form = document.querySelector("form");
+
+// form submission to insert meme in section
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+})
