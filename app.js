0b5c45b5881ac7660d5b535331ddeea59cbd6ee4
Engines-screaming <karl.ravago@gmail.com>
2021-09-21 22:58:34 -0700

    meme generation attempt. Not detecting iinput text when submitted

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
 // selectors
-const image = document.querySelector("imgLink");
-const topText = document.querySelector("topText");
-const botText = document.querySelector("botText");
-const form = document.querySelector("form");
+document.addEventListener("DOMContentLoaded", function() {
+    const image = document.querySelector("#imgLink");
+    const inputTopText = document.querySelector("#topText");
+    const inputBotText = document.querySelector("#botText");
+    const form = document.querySelector("form");
+    const memeSection = document.querySelector("section");
 
-// form submission to insert meme in section
-form.addEventListener("submit", function(e) {
-    e.preventDefault();
+    // function to create a meme
+    function createMeme(imgLink, topText, botText) {
+        const newMeme = document.createElement("div");
+
+        const top = document.createElement("h1");
+        top.textContent = inputTopText.value;
+
+        const bot = document.createElement("h1");
+        bot.textContent = inputBotText.value;
+
+        const img = document.createElement("img");
+        img.setAttribute("src", image.value);
+
+        newMeme.append(img, top, bot);
+        memeSection.append(newMeme);
+    }
+
+    // form submission to insert meme in section
+    form.addEventListener("submit", function(e) {
+        e.preventDefault();
+
+        createMeme(image.value, inputTopText.value, inputBotText.value);
+
+    })
+
+
 })
