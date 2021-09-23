2a4713d3de7fc09692d11343a198c91ce7ca85ba
Engines-screaming <karl.ravago@gmail.com>
2021-09-22 20:03:15 -0700

    able to create memes and add them to section. iadded border around text. :

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
 // selectors
-document.addEventListener("DOMContentLoaded", function() {
-    const image = document.querySelector("#imgLink");
-    const inputTopText = document.querySelector("#topText");
-    const inputBotText = document.querySelector("#botText");
-    const form = document.querySelector("form");
-    const memeSection = document.querySelector("section");
+const image = document.querySelector("#imgLink");
+const inputTopText = document.querySelector("#topText");
+const inputBotText = document.querySelector("#botText");
+const form = document.querySelector("form");
+const memeSection = document.querySelector("section");
 
-    // function to create a meme
-    function createMeme(imgLink, topText, botText) {
-        const newMeme = document.createElement("div");
+// function to create a meme
+function createMeme(imgLink, topText, botText) {
+    const newMeme = document.createElement("div");
+    newMeme.setAttribute("class", "container");
 
-        const top = document.createElement("h1");
-        top.textContent = inputTopText.value;
+    const top = document.createElement("h1");
+    top.textContent = inputTopText.value;
+    top.setAttribute("class", "top-text");
 
-        const bot = document.createElement("h1");
-        bot.textContent = inputBotText.value;
+    const bot = document.createElement("h1");
+    bot.textContent = inputBotText.value;
+    bot.setAttribute("class", "bottom-text");
 
-        const img = document.createElement("img");
-        img.setAttribute("src", image.value);
+    const img = document.createElement("img");
+    img.setAttribute("src", image.value);
 
-        newMeme.append(img, top, bot);
-        memeSection.append(newMeme);
-    }
+    newMeme.append(img, top, bot);
+    memeSection.append(newMeme);
+}
 
-    // form submission to insert meme in section
-    form.addEventListener("submit", function(e) {
-        e.preventDefault();
+// form submission to insert meme in section
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
 
-        createMeme(image.value, inputTopText.value, inputBotText.value);
-
-    })
-
-
+    createMeme(image.value, inputTopText.value, inputBotText.value);
 })
+
+
