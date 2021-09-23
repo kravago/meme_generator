b0c0c359ccf9240f2e14b3a221e6823e77e0eb17
Engines-screaming <karl.ravago@gmail.com>
2021-09-22 22:15:11 -0700

    added button in top left of meme to rdelete a meme

--- app.js
+++ app.js
@@ -7,21 +7,31 @@ const memeSection = document.querySelector("section");
 
 // function to create a meme
 function createMeme(imgLink, topText, botText) {
+    // div containing meme
     const newMeme = document.createElement("div");
     newMeme.setAttribute("class", "container");
 
+    // top text
     const top = document.createElement("h1");
     top.textContent = inputTopText.value;
     top.setAttribute("class", "top-text");
 
+    // bot text
     const bot = document.createElement("h1");
     bot.textContent = inputBotText.value;
     bot.setAttribute("class", "bottom-text");
 
+    // meme img
     const img = document.createElement("img");
     img.setAttribute("src", image.value);
 
-    newMeme.append(img, top, bot);
+    // delete button
+    const delButton = document.createElement('button');
+    delButton.textContent = "X";
+    delButton.setAttribute("class", "delete-button");
+    delButton.addEventListener("click", deleteMeme);
+
+    newMeme.append(delButton, img, top, bot);
     memeSection.append(newMeme);
 
     // clear form
@@ -30,11 +40,13 @@ function createMeme(imgLink, topText, botText) {
     image.value = "";
 }
 
+// handler for deleting images
+function deleteMeme(e) {
+    e.target.parentElement.remove();
+}
+
 // form submission to insert meme in section
 form.addEventListener("submit", function(e) {
     e.preventDefault();
-
     createMeme(image.value, inputTopText.value, inputBotText.value);
 })
-
-
